dbb81d8c0a5229ef6312590be53c358ddc9615af
HuynhThaiHCMUT <thai.dinhhuynh@hcmut.edu.vn>
2024-04-05 19:06:24 +0700

    fix traffic by time

--- backend/report/views.py
+++ backend/report/views.py
@@ -2,58 +2,74 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-from report.models import Metadata
-from report.serializers import MetadataSerializer
-from datetime import datetime
+from report.models import Camera, Metadata
+from report.serializers import CameraSerializer, MetadataSerializer
+from datetime import datetime, timedelta
 
+
+# Query parameters:
+#   day: %d-%m-%y
+#   type: day/week/month
+#
+# Response:
+#   {
+#       "%camera_id": [[people in 1, people out 1], [people in 2, people out 2],... [people in n, people out n]],
+#       "%camera_id": [[people in 1, people out 1], [people in 2, people out 2],... [people in n, people out n]],
+#   }
+#
+# Với n = 24/7/30 khi type = day/week/month
+#
 @api_view(["GET"])
 def traffic_by_time(request: Request):
-    start = request.query_params.get("start")
-    end = request.query_params.get("end")
-    typeGraph = request.query_params.get("typeGraph")
+    date_string = request.query_params.get("day", None)
+    graph_type = request.query_params.get("type", "day")
     
     # Chuyển đổi chuỗi ngày/tháng/năm thành đối tượng datetime
-    start = datetime.strptime(start, "%d-%m-%Y")
-    end = datetime.strptime(end, "%d-%m-%Y")
-    # Chuyển đổi đối tượng datetime thành chuỗi định dạng ISO 8601
-    start = start.isoformat()
-    end = end.isoformat()
+    date = datetime.strptime(date_string, "%d-%m-%Y") if date_string else datetime.now()
     
-    mappingForType = {
+    mapping = {
         "day": [24,1],
         "week": [7,24],
         "month": [30,24]
     }
-    
-    trafficByTime = []
-    if typeGraph in mappingForType:
-        rangeFor,timeAdd = mappingForType[typeGraph]
-        for i in range(rangeFor):
-            startTmp = start + datetime.timedelta(hours=i)
-            endTmp = startTmp + datetime.timedelta(hours=i+timeAdd)
-            serializer = MetadataSerializer(Metadata.objects.filter(time__gte=startTmp, time__lte=endTmp), many=True)
-        listCam = {}
-        for i in serializer.data:
-            if i.get("camera") in listCam:
-                listCam[i.get("camera")] += [i]
-            else: 
-                listCam[i.get("camera")] = [i]
-        for key, value in listCam.items():
-            filtered_value = [value[0], value[-1]]
-            listCam[key] = filtered_value
-        trafficByTime+=[listCam]
+    # Tính ngày bắt đầu
+    start = date - (timedelta(days=1) if (graph_type == "day") else timedelta(days=mapping[graph_type][0]))
+    data = []
+
+    if graph_type in mapping:
+        range_for, time_add = mapping[graph_type]
+        #data = [[Data trong khoảng 1], [Data trong khoảng 2],... [Data trong khoảng n]]
+        for i in range(range_for):
+            start_time = start + timedelta(hours=i)
+            end_time = start + timedelta(hours=i+time_add)
+            serializer = MetadataSerializer(Metadata.objects.filter(time__gte=start_time, time__lte=end_time), many=True)
+            data += [serializer.data]
+
+        list_cam = {}
+        camera_serializer = CameraSerializer(Camera.objects.all(), many=True)
+        for camera in camera_serializer.data:
+            list_cam[camera["id"]] = []
+
+        for section in data:
+            for value in list_cam.values():
+                value += [[]]
+            for i in section:
+                list_cam[i.get("camera")][-1] += [i]    
+        #list_cam = {"1": [[Data 1], [Data 2],... [Data n]], "2": [[Data 1], [Data 2],... [Data n]]}
+            
+        for key, sections in list_cam.items():
+            filtered_value = []
+            for i in sections:
+                if len(i) >= 1:
+                    filtered_value += [(i[0]["people_in"]-i[-1]["people_in"], i[0]["people_out"]-i[-1]["people_out"])]
+                else:
+                    filtered_value += [None]
+            list_cam[key] = filtered_value
         
-        return Response(serializer.data, status.HTTP_200_OK)
+        return Response(list_cam, status.HTTP_200_OK)
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
-        
-    
-    # listCam trả về dict của các camera cấu trúc { <id của camera> :[matadata max, metadate min]}
-    # neu type la day res cos danjg [{}{}{}...] 24 entry 1 entry tuon ung { <id của camera> :[matadata max, metadate min]}
-    # neu type la week res cos danjg [{}{}{}...] 7 entry 1 entry tuon ung { <id của camera> :[matadata max, metadate min]}
-    
-    
-    
+
 
 @api_view(["GET"])
 def most_least_traffic(request: Request):
