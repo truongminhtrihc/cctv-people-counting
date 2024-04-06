e50b831e2132d2cf6b31b04d8ce8de1185a62968
truongminhtrihc <truongminhtringuyenvantu@gmail.com>
2024-04-06 22:39:21 +0700

    mosttrafficv1

--- backend/report/views.py
+++ backend/report/views.py
@@ -72,20 +72,71 @@ def traffic_by_time(request: Request):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+
+# tìm trong 1 ngày/ 1 tuần/ 1 tháng khoảng thời gian nào (giờ  / ngày / ngày ) có số lượng người ra/vào  lớn/nhỏ nhất tương ứng từng camera
 @api_view(["GET"])
 def most_least_traffic(request: Request):
-    time_span = request.query_params.get("by", "day")
-    if time_span == "day":
-        time = datetime.datetime.now() - datetime.timedelta(days=1)
-    elif time_span == "week":
-        time = datetime.datetime.now() - datetime.timedelta(days=7)
-    elif time_span == "month":
-        time = datetime.datetime.now() - datetime.timedelta(days=30)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    date_string = request.query_params.get("day", None)
+    time_type = request.query_params.get("timetype", "day")
+    inOrOut_type = request.query_params.get("inouttype", "in")
+    maxOrMin_type = request.query_params.get("maxormin","max")
+    # Chuyển đổi chuỗi ngày/tháng/năm thành đối tượng datetime
+    date = datetime.strptime(date_string, "%d-%m-%Y") if date_string else datetime.now()
     
-    serializer = MetadataSerializer(Metadata.objects.filter(time__gte = time), many=True)
-    return Response(serializer.data, status.HTTP_200_OK)
+    mapping = {
+        "day": [24, 1],
+        "week": [7, 24],
+        "month": [30, 24]
+    }
+    # Tính ngày bắt đầu
+    start = date - (timedelta(days=1) if (time_type == "day") else timedelta(days=mapping[time_type][0]))
+    data = []
+    list_cam = {}
+    traffic_by_time = {}
+
+    if time_type in mapping:
+        range_for, time_add = mapping[time_type]
+        #data = [[Data trong khoảng 1], [Data trong khoảng 2],... [Data trong khoảng n]]
+        for i in range(range_for):
+            start_time = start + timedelta(hours=i*time_add)
+            end_time = start + timedelta(hours=(i+1)*time_add)
+            serializer = MetadataSerializer(Metadata.objects.filter(time__gte=start_time, time__lte=end_time), many=True)
+            data += [serializer.data]
+        camera_serializer = CameraSerializer(Camera.objects.all(), many=True)
+        for camera in camera_serializer.data:
+            list_cam[camera["id"]] = []
+
+        for section in data:
+            for value in list_cam.values():
+                value += [[]]
+            for i in section:
+                list_cam[i.get("camera")][-1] += [i]    
+        for key, sections in list_cam.items():
+            traffic_by_time[key] = [[], []]
+            for i in sections:
+                if len(i) >= 1:
+                    filtered_value = [i[0]["people_in"]-i[-1]["people_in"], i[0]["people_out"]-i[-1]["people_out"]]
+                else:
+                    filtered_value = [0, 0]
+                traffic_by_time[key][0] += [filtered_value[0]]
+                traffic_by_time[key][1] += [filtered_value[1]]
+        
+        
+        mapping_most = {
+        "in": 0,
+        "out":1,
+        "max": max,
+        "min": min
+    }
+        most_traffic = {}
+        for camera_id, records in traffic_by_time.items():
+            max_pair = mapping_most[maxOrMin_type](records, key=lambda x: x[mapping_most[inOrOut_type]])
+            most_traffic[camera_id] = max_pair
+
+        
+        return Response(most_traffic, status.HTTP_200_OK)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET"])
 def camera(request: Request):
