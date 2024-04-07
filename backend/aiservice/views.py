6f234ed6f3220fecce44c2469ed9da74c6be5217
HuynhThaiHCMUT <thai.dinhhuynh@hcmut.edu.vn>
2024-04-07 11:20:34 +0700

    add additional stream check

--- backend/aiservice/views.py
+++ backend/aiservice/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 import shutil
 import subprocess
@@ -7,29 +8,83 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
-from report.serializers import MetadataSerializer
+from report.models import Camera
+from report.serializers import CameraSerializer, MetadataSerializer
 
 storage = settings.MEDIA_ROOT
 ffmpeg_processes = dict()
 
+"""
+Query parameter:
+    camera_id: str
+    refresh: 1/None
+
+Response:
+    string link to hls stream file
+"""
 @api_view(["GET"])
 def stream_url(request: Request):
     camera_id = request.query_params.get("id")
-    if camera_id and os.path.exists(storage + camera_id):
-        return Response("media/" + camera_id + "/output.m3u8", status.HTTP_200_OK)
-    else:
+    refresh = request.query_params.get("refresh")
+    try:
+        serializer = CameraSerializer(Camera.objects.get(id=camera_id))
+    except:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    global ffmpeg_processes
+    if check_stream(serializer.data.get("ip")):
+        if camera_id in ffmpeg_processes:
+            return Response("media/" + camera_id + "/output.m3u8", status.HTTP_200_OK)
+        if refresh:
+            create_stream(serializer.data.get("ip"), camera_id)
+            return Response("media/" + camera_id + "/output.m3u8", status.HTTP_200_OK)
+    return Response(status=status.HTTP_408_REQUEST_TIMEOUT)
 
+"""
+Request:
+{
+    "port": "%camera_port"
+    "camera_id": "%camera_id"
+}
+"""
 @api_view(["POST"])
-def stream(request: Request):
-    ip = request.data.get("ip")
-    camera_id = request.data.get("id")
-    shutil.rmtree(storage + camera_id, ignore_errors=True)
-    os.makedirs(storage + camera_id)
+def stream(request: HttpRequest):
+    port = str(request.data.get("port"))
+    camera_id = str(request.data.get("id"))
+    ip = 'rtsp://' + request.META.get("REMOTE_ADDR") + ':' + port + '/'
+    if check_stream(ip):
+        try:
+            CameraSerializer(Camera.objects.get(id=camera_id), data={ip: ip}, partial=True)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        create_stream(ip, camera_id)
+        return Response(status=status.HTTP_201_CREATED)
+    else:
+        return Response(status=status.HTTP_408_REQUEST_TIMEOUT)
+
+"""
+Request:
+{
+    "camera": %i,
+    "people_in": %i,
+    "people_out": %i
+}
+"""
+@api_view(["POST"])
+def metadata(request: Request):
+    serializer = MetadataSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+def create_stream(ip: str, camera_id: str):
     command = [
         'ffmpeg',
         '-i', ip,
-        '-c:v', 'copy',
+        '-vf', 'select=concatdec_select',
+        '-af', 'aselect=concatdec_select,aresample=async=1',
         '-c:a', 'aac',
         '-f', 'hls',
         '-hls_time', '10',
@@ -40,23 +95,16 @@ def stream(request: Request):
 
     global ffmpeg_processes
     if camera_id not in ffmpeg_processes:
+        shutil.rmtree(storage + camera_id, ignore_errors=True)
+        os.makedirs(storage + camera_id)
         ffmpeg_processes[camera_id] = subprocess.Popen(command)
     else:
-        ffmpeg_processes[camera_id].kill
+        ffmpeg_processes[camera_id].kill()
+        shutil.rmtree(storage + camera_id, ignore_errors=True)
+        os.makedirs(storage + camera_id)
         ffmpeg_processes[camera_id] = subprocess.Popen(command)
-    return Response(status=status.HTTP_201_CREATED)
 
-# Request:
-# {
-#   "camera": %i,
-#   "people_in": %i,
-#   "people_out": %i
-# }
-@api_view(["POST"])
-def metadata(request: Request):
-    serializer = MetadataSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+def check_stream(ip: str) -> bool:
+    command = ['ffprobe', '-timeout', '1000000', ip]
+    process = subprocess.run(command)
+    return process.returncode == 0
--- backend/report/views.py
+++ backend/report/views.py
@@ -6,19 +6,19 @@ from report.models import Camera, Metadata
 from report.serializers import CameraSerializer, MetadataSerializer
 from datetime import datetime, timedelta
 
+"""
+Query parameters:
+    day: %d-%m-%y
+    type: day/week/month
 
-# Query parameters:
-#   day: %d-%m-%y
-#   type: day/week/month
-#
-# Response:
-#   {
-#       "%camera_id": [[people in 1, people out 1], [people in 2, people out 2],... [people in n, people out n]],
-#       "%camera_id": [[people in 1, people out 1], [people in 2, people out 2],... [people in n, people out n]],
-#   }
-#
-# Với n = 24/7/30 khi type = day/week/month
-#
+Response:
+{
+    "%camera_id1": [[people in 1, people in 2,...people in n], [people out 1, people out 2,...people out n]]
+    "%camera_id2": [[people in 1, people in 2,...people in n], [people out 1, people out 2,...people out n]],
+}
+
+Với n = 24/7/30 khi type = day/week/month
+"""
 @api_view(["GET"])
 def traffic_by_time(request: Request):
     date_string = request.query_params.get("day", None)
@@ -60,13 +60,16 @@ def traffic_by_time(request: Request):
         for key, sections in list_cam.items():
             traffic_by_time[key] = [[], []]
             for i in sections:
-                if len(i) >= 1:
+                if len(i) > 1:
                     filtered_value = [i[0]["people_in"]-i[-1]["people_in"], i[0]["people_out"]-i[-1]["people_out"]]
+                elif len(i) == 1 and prev != None:
+                    filtered_value = [i[0]["people_in"]-prev[-1]["people_in"], i[0]["people_out"]-prev[-1]["people_out"]]
                 else:
                     filtered_value = [0, 0]
                 traffic_by_time[key][0] += [filtered_value[0]]
                 traffic_by_time[key][1] += [filtered_value[1]]
-                
+                prev = i
+
         return Response(traffic_by_time, status.HTTP_200_OK)
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -127,7 +130,7 @@ def most_least_traffic(request: Request):
         "out":1,
         "max": max,
         "min": min
-    }
+        }
         most_traffic = {}
         for camera_id, records in traffic_by_time.items():
             max_pair = mapping_most[maxOrMin_type](records, key=lambda x: x[mapping_most[inOrOut_type]])
@@ -137,6 +140,21 @@ def most_least_traffic(request: Request):
         return Response(most_traffic, status.HTTP_200_OK)
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+Query parameter:
+    type: day/week/month
+    range: %i
+
+Response:
+[Average 1, Average 2,... Average n]
+
+Với n = 24/7/30 khi type = day/week/month
+"""
+@api_view(["GET"])
+def average_traffic(request: Request):
+    pass
 
 @api_view(["GET"])
 def camera(request: Request):
